c62998e92bf6606defaf0240d3c372640c7e29bb
Johanes Zenn <johannes.zenn@icloud.com>
2020-03-24 18:17:02 +0100

    plugin remote_file, fetches a document for a page

---
+++ _plugins/remote_file.rb
@@ -0,0 +1,36 @@
+require 'nokogiri'
+require 'open-uri'
+require 'uri'
+
+# this is heavily inspired by https://stackoverflow.com/a/14963269/11262791
+
+module Jekyll
+
+  class RawGithubContent < Liquid::Tag
+
+    def initialize(tag_name, url, tokens)
+
+      # check if the URL is valid
+      if url =~ URI::regexp
+
+        # grab the remote document with nokogiri
+        doc = Nokogiri::HTML(open(url))
+
+        # get the text inside body
+        @node = doc.xpath('//text()')
+
+      else 
+        @node = 'Could not fetch any content.'
+      end
+
+      super
+    end
+
+    def render(context)
+      output = super
+      @node.to_s
+    end
+  end
+end
+
+Liquid::Template.register_tag('remote_file', Jekyll::RawGithubContent)
